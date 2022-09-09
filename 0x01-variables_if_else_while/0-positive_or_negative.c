f36c57986c451387bacb7216399361f6d0092c71
awuni-a <awuni.alexander@gmail.com.com>
2022-09-09 18:43:06 +0000

    assign a random number to the variable

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
 /**
 * main - print if the random number is positive, zero or negative
 *
@@ -22,7 +23,7 @@ if (n > 0)
 	printf("%i is positive\n", n);
 }
 else if (n == 0)
-{
+:i{
 	printf("%i is zero\n", n);
 }
 else if (n < 0)
