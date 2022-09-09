32387b2cdef78c322fa9c1647b3e702df158d3ee
awuni-a <awuni.alexander@gmail.com.com>
2022-09-09 18:26:44 +0000

    assign a random number to the variable

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,8 +16,8 @@ int n;
  
 srant(time(0));
 n = rant() - RAND_MAX / 2;
-	/* your codes goes there*/
-	if if (n > 0)
+  /* your codes goes there*/
+if if (n > 0)
 {
 	printf("%i is positive\n", n);
 }
