8991b2d53189a21d0db5602b27b7cafc9059bbed
awuni-a <awuni.alexander@gmail.com>
2022-09-10 04:04:36 +0000

    program that prints the alphabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - print if the random number is positive, zero or negative
+*
+* Description: using the main function
+* this program prints "number is positive, zero or negative"
+*
+* Return: 0
+*/
+int main(void)
+{
+char alphagame;
+for (alphagame = "a"; alphagame <= "z"; alphagame++)
+{
+	putchar(alphagame);
+}
+putchar("\n");
+return (0);
+}
