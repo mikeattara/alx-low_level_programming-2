998ff246aea9b325d6aa5854f430a8b73d590e73
awuni-a <awuni.alexander@gmail.com.com>
2022-09-13 18:26:47 +0000

    fix

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * print_alphabet - Entry point
+ * print the alphabet, in lowercase 
  *
- * Return: Always 0 (Success)
+ * Return: Always 0.
  */
 void print_alphabet(void)
 {
@@ -11,5 +11,5 @@ void print_alphabet(void)
 	{
 		_putchar(alphabet);
 	}
-	_putchar('\n');
+_putchar('\n');
 }
