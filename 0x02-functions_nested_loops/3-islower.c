b2b371020544cc0512dede2dca50dabb32a60200
awuni-a <awuni.alexander@gmail.com.com>
2022-09-13 20:59:16 +0000

    a function that checks for lowercase character

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* _islower - A function to check for lowercase alphabet
+*
+* Return: Always 0.
+*/
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
