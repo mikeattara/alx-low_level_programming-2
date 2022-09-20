8f64df69e3cedcf6ef00bf2f21f7f29a7ec2900c
awuni-a <awuni.alexander@gmail.com.com>
2022-09-20 05:21:43 +0000

    a function that prints every other character of a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * puts2 -  a function that prints every other character of a string.
+ *
+ * @str: This parameter is passed.
+ *
+ * Return: Always a success.
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != 0)
+	{
+		if ((i % 2) == 0)
+			putchar(str[i]);
+		i++;
+	}
+	putchar('\n');
+}
