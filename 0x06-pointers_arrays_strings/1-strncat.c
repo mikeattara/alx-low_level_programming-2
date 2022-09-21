4f45150316d46e8ac356fa0ddd955d01a492af15
awuni-a <awuni.alexander@gmail.com.com>
2022-09-21 03:32:48 +0000

    fix

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,21 +10,18 @@
  * @n: input of max bytes to be used
  * Return: Always a success
  */
+
 char *_strncat(char *dest, char *src, int n)
 {
-	int i, j;
+	int index = strlen(dest);
+	int a = 0;
 
-	for (i = 0; dest[i] != '\0'; i++)
+	while (a < n && *src)
 	{
-
+		dest[index + a] = *src;
+		src++;
+		a++;
 	}
-
-	for (j = 0; co < n; j++)
-	{
-		dest[i + co] = src[j];
-		if (src[j] == '\0')
-			j = n;
-	}
-
+	dest[index + a] = '\0';
 	return (dest);
 }
