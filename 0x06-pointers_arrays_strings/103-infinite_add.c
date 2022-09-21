389726a7a865aa0a28413717095a377fe325904c
awuni-a <awuni.alexander@gmail.com.com>
2022-09-21 03:04:51 +0000

    fix

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -2,27 +2,28 @@
 
 /**
  * rev_string - a function that adds two numbers
- * @n: integer parameter
+ * @s: int parameter
  *
  * Return: Always 0
  */
 
-void rev_string(char *n)
+char *rot13(char *s)
 {
-	int i = 0;
-	int j = 0;
-	char temp;
+	int i, j;
 
-	while (*(n + i) != '\0')
-	{
-		i++;
-	}
-	i--;
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-	for (j = 0; j < i; j++, i--)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		temp = *(n + j);
-		*(n + j) = *(n + i);
-		*(n + i) = temp;
+		for (j = 0; a[j] != '\0'; j++)
+		{
+			if (s[i] == a[j])
+			{
+				s[i] = b[j];
+				break;
+			}
+	    }
 	}
+	return (s);
 }
