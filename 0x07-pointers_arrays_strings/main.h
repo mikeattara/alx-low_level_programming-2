c9582633dbdea602262d61f1e3b4a6dc883670fc
awuni-a <awuni.alexander@gmail.com.com>
2022-09-26 23:23:11 +0000

    fix

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_FILR
-#define MAIN_FILE
+#ifndef MAIN_H
+#define MAIN_H
 
 int_putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
@@ -12,4 +12,4 @@ void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
 
-#endif /*MAIN_FILE*/
+#endif /*MAIN_H*/
