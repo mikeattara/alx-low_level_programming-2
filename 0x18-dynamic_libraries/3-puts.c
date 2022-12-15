e6bb6a026e1e0f91ac378fc73e2e8111e1c38d8a
awuni-a <awuni.alexander@gmail.com>
2022-12-15 08:54:35 +0000

    A library is not a luxury

---
+++ 0x18-dynamic_libraries/3-puts.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* _puts - function that prints a string, followed by a new line, to stdout
+* @str: pointer type char
+* Return: Always 0 (Success)
+*/
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0';)
+	{
+		putchar(str[i]);
+		str++;
+	}
+	putchar('\n');
+}
